aa10bdbb4459b5e0d39a43044989512bd34e1cdb
Himanshu Patil <dev@himanshupatil.dev>
2025-11-15 16:09:46 +0530

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 
 import preact from "@astrojs/preact";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://human-writes.himanshupatil.dev",
-  integrations: [preact()],
+  integrations: [preact(), sitemap()],
 });
